0b422e418dd272828b81be789cf63d9199855308
Shofiullah Babor <babu_12f@yahoo.com>
2020-04-03 20:25:25 +0600

    use of jstl -55

--- src/main/java/com/babor/spring/web/controllers/NoticesController.java
+++ src/main/java/com/babor/spring/web/controllers/NoticesController.java
@@ -1,22 +1,17 @@
 package com.babor.spring.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Map;
 
 @Controller
 public class NoticesController {
 
     @RequestMapping("/")
-    public ModelAndView showHome() {
-        ModelAndView mv = new ModelAndView("home");
+    public String showHome(Model model) {
+        model.addAttribute("name", "<b>jhon doe</b>");
 
-        Map<String, Object> model = mv.getModel();
-        model.put("name", "jhon doe");
-
-        return mv;
+        return "home";
     }
 
 }
--- src/main/webapp/WEB-INF/jsps/home.jsp
+++ src/main/webapp/WEB-INF/jsps/home.jsp
@@ -1,14 +1,15 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Spring-mvc start</title>
 </head>
 <body>
-    <h2>
-        <%= request.getAttribute("name") %>
-    </h2>
-    <h2>
-        using expression language: ${name}
-    </h2>
+    <p>
+        using (jstl) <c:out value="${name}"></c:out>
+    </p>
+    <p>
+        using (EL): ${name}
+    </p>
 </body>
