0a3249e40494e66dd9702db8d4c6f5b777271594
Shofiullah Babor <babu_12f@yahoo.com>
2020-04-04 18:49:42 +0600

    add more validation tag -68

--- src/main/java/com/babor/spring/web/dao/Notice.java
+++ src/main/java/com/babor/spring/web/dao/Notice.java
@@ -1,12 +1,21 @@
 package com.babor.spring.web.dao;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Notice {
     private int id;
-    @Size(min=4, max=100)
+
+    @Size(min=4, max=100, message = "Name must be between 4 and 100 characters")
     private String name;
+
+    @NotNull
+    @Pattern(regexp = ".*\\@.*\\..*", message = "Not a valid email address")
     private String email;
+
+    @Size(min = 20, max = 255, message = "Notice must be between 20 and 255 characters")
     private String text;
 
     public Notice() {
