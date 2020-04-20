93c02924167cd6c316393e7f9ee1d8cf24133a56
Shofiullah Babor <babu_12f@yahoo.com>
2020-04-20 20:31:25 +0600

    hibernate criteria -140

--- src/main/java/com/babor/spring/web/dao/UserDAO.java
+++ src/main/java/com/babor/spring/web/dao/UserDAO.java
@@ -1,25 +1,21 @@
 package com.babor.spring.web.dao;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 @Repository
 @Component("userDao")
 @Transactional
 public class UserDAO {
-    private NamedParameterJdbcTemplate jdbc;
-
     @Autowired
     private PasswordEncoder passwordEncoder;
 
@@ -27,11 +23,6 @@ public class UserDAO {
     private SessionFactory sessionFactory;
 
     public UserDAO() {
-    }
-
-    @Autowired
-    public void setDataSource(DataSource dataSource) {
-        this.jdbc = new NamedParameterJdbcTemplate(dataSource);
     }
 
     public Session session() {
@@ -45,15 +36,19 @@ public class UserDAO {
         session().save(user);
     }
 
-
     public boolean exists(String username) {
-        return jdbc.queryForObject("select count(*) from users where username = :username",
-                new MapSqlParameterSource("username", username), Integer.class) > 0;
+        Criteria criteria = session().createCriteria(User.class);
 
+//        criteria.add(Restrictions.eq("username", username));
+        criteria.add(Restrictions.idEq(username));
+
+        User user = (User) criteria.uniqueResult();
+
+        return user != null;
     }
 
-    @SuppressWarnings({"UnnecessaryLocalVariable", "unchecked"})
+    @SuppressWarnings("JpaQlInspection")
     public List<User> getAll() {
-        return session().createQuery("from User3").list();
+        return session().createQuery("from User").list();
     }
 }
